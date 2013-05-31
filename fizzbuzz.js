1e00fa7d52f79318030ae19982dd5e0a2b509bf7
bayousoft <bayousoft@gmail.com>
2013-05-31 18:01:14 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,15 +1,1 @@
-for (var i=0;i<100;i++)
-{ 
-    if (i % 3 && i % 5) {
-        document.write('FizzBuzz' + '<br/>');
-    } else if (i % 3) 
-    {
-        document.write('Fizz' + '<br/>');    
-    } else if (i % 5) 
-    {
-        document.write('Buzz' + '<br/>');    
-    } else 
-    {
-        document.write(i + '<br/>');
-    }
-}
+.
